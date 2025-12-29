70b5fc74e9cf71dc362d95b3ac45df6c850c52f9
nivilwilsonp <nivilwilsonp@gmail.com>
2025-12-29 07:46:36 +0530

    fetch data from api and display on poster

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 import Poster from '../Poster/Poster'
+import axios from 'axios'
+import { baseURL } from '../constants';
 
 function Home() {
+  const [ImgUrl,setImageUrl]=useState("");
+  const[MovieData,setMovieData]=useState({});
+  useEffect(()=>{
+    
+  const url=`${baseURL}/api/trending`;
+ axios.get(url).then(response => {
+  if(response.data.results.length>0){
+    
+let data=response.data.results[1];
+console.log("the data is = ",data.backdrop_path);
+setImageUrl(data.backdrop_path);
+setMovieData(data);
+
+  }else{
+    console.log("No trending movies found");
+    
+  }
+    
+ }).catch(error => {
+    console.error('There was an error!', error)
+ })   ;},[]);
+
   return (
     <div>
-<Poster backgroundImage="/background.webp"></Poster>
+   {ImgUrl && <Poster path={ImgUrl} data={MovieData} />}
     </div>
   )
 }
--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -3,18 +3,25 @@ import "./Poster.css";
 import axios from '../Axios';
 import "@fontsource/oswald/700.css";
 import "@fontsource/oswald/400.css";
+import { baseURL } from '../constants';
 
 function Poster(props) {
-  const { backgroundImage} = props;
+  
+  //const { path="/iN41Ccw4DctL8npfmYg1j5Tr1eb.jpg", size = "original" } = props;   
+    const { path,data, size = "w1280" } = props;   
+                                    
+
+  const url = `${baseURL}/api/image?path=${path}&size=${size}`;
+  
 
   return (
 
     <div
       className="poster"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
+      style={{ backgroundImage: `url(${url})`, backgroundSize: 'cover' }}
     >
-        <h1 className='poster-title'>Movie name.</h1>
-        <h2 className='poster-description'>description</h2>
+        <h1 className='poster-title'>{data.title}.</h1>
+        <h2 className='poster-description'>{data.overview}</h2>
         <div className='poster-buttons'>
             <button className='poster-button play-button'>Play</button>
             <button className='poster-button info-button'>More Info</button>
